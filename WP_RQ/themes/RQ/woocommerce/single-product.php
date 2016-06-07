b33988c651c80e93223e5a93942876b681e6e445
PetyaSh <petyash@i.ua>
2016-06-08 00:06:31 +0300

    no message

--- WP_RQ/themes/RQ/woocommerce/single-product.php
+++ WP_RQ/themes/RQ/woocommerce/single-product.php
@@ -1,5 +1,22 @@
 <?php if ( ! defined( 'ABSPATH' ) ) { exit; } get_header( 'shop' ); while ( have_posts() ) : the_post(); global $product; ?>
 
+<?php
+global $product;
+$title = get_the_title();
+
+// prices
+$price = $product->get_price();
+
+// images
+$img = get_field('_thumbnail_id');
+$gallery = get_field('_gallery');
+
+// characters
+$buttle_volume = get_field('buttle_volume');
+$pack_count = get_field('pack_count');
+$hover_text = get_field('hover_text');
+?>
+
 <section id="one-page">
     <div id="top"<?php $product_bg = get_field('product_bg', 32); if(is_array($product_bg) && count($product_bg)): ?> style="background: url('<?php echo $product_bg['sizes']['large']; ?>') no-repeat 50% 50%;"<?php endif; ?>>
         <div class="box">
@@ -16,155 +33,98 @@
             <div class="left top-left">
                 <div id="prod-slider">
                     <div class="short">
-                        <span class="vol">0,8l</span>
-                        <span class="qty">12 шт в упаковке</span>
+                        <?php if($buttle_volume): ?>
+                            <span class="vol"><?php echo $buttle_volume; ?><?php echo __('L', 'RQ'); ?></span>
+                        <?php endif; ?>
+                        <?php if($pack_count): ?>
+                            <span class="qty"><?php echo $pack_count; ?> <?php echo __('pcs.', 'RQ'); ?> <?php echo __('in pack', 'RQ'); ?></span>
+                        <?php endif; ?>
                     </div>
-                    <ul class="slides">
-                        <li><img src="img/bottle.jpg" alt=""></li>
-                        <li><img src="img/bottle.jpg" alt=""></li>
-                    </ul>
+
+                    <?php if(is_array($img) && count($img)): ?>
+                        <ul class="slides">
+                            <li><img src="<?php echo $img['sizes']['600x0']; ?>" alt='<?php echo $title; ?>'></li>
+                            <?php if(is_array($gallery) && count($gallery)): ?>
+                                <?php foreach($gallery as $img): ?>
+                                    <li><img src="<?php echo $img['sizes']['600x0']; ?>" alt='<?php echo $title; ?>'></li>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </ul>
+                    <?php endif; ?>
                 </div>
             </div>
+
             <div class="right top-right">
                 <div class="order">
-                    <p class="header">Минеральная вода RusseQuelle <br> 0,7 л (упаковка: стекло)</p>
-                    <div class="qty">
-                        12 шт. в упаковке
-                        <div class="what">?
-                            <div class="hint">
-                                <p>В стандартной упаковке 12 стеклянных бутылок по 0,7 л</p>
-                            </div>
+                    <p class="header"><?php echo $title; ?></p>
+
+                    <?php if($pack_count): ?>
+                        <div class="qty">
+                            <?php echo $pack_count; ?> <?php echo __('pcs.', 'RQ'); ?> <?php echo __('in pack', 'RQ'); ?>
+                            <?php if($hover_text): ?><div class="what">?<div class="hint"><p><?php echo $hover_text; ?></p></div></div><?php endif; ?>
                         </div>
-                    </div>
-                    <div class="price">
-                        <span>цена за </span>
-                        <a href="#qty" class="select" data-popup>12</a>
-                        <span>шт</span>
-                        <span class="uah">900 р.</span>
-                    </div>
+                    <?php endif; ?>
+
+                    <?php if($price && $product->is_purchasable() && $product->is_in_stock()): ?>
+                        <div class="price">
+                            <span><?php echo __('price for', 'RQ'); ?> </span><a href="#qty" class="select" data-popup><?php echo $pack_count; ?></a><span><?php echo __('pcs.', 'RQ'); ?></span>
+                            <span class="uah"><?php echo $price; ?> <?php echo get_woocommerce_currency_symbol(); ?></span>
+                        </div>
+                    <?php endif; ?>
+
                     <ul class="btn-group">
-                        <li><a href="#" class="add">Добавить в корзину</a></li>
-                        <li><a href="#">Заказать обратный звонок</a></li>
-                        <li><a href="#">Онлайн чат с менеджером</a></li>
+                        <?php if($price && $product->is_purchasable() && $product->is_in_stock()): ?>
+                            <li><a href="javascript:void(0)" class="add"><?php echo __('Add to cart', 'RQ'); ?></a></li>
+                        <?php endif; ?>
+                        <li><a href="javascript:void(0)"><?php echo __('Call back', 'RQ'); ?></a></li>
+                        <li><a href="javascript:void(0)"><?php echo __('Online chat with the manager', 'RQ'); ?></a></li>
                     </ul>
+
                     <a href="#terms" data-popup class="terms">условия оплаты и доставки</a>
                 </div>
             </div>
+
             <div class="clearfix"></div>
-            <div class="left">
-                <div class="text">
-                    <h3>Движение воды RusseQuelle происходит через пласты пород толщиной в несколько сотен метров.</h3>
-                    <p>В основу концепции RusseQuelle заложен принцип абсолютной чистоты. Источник имеет историю более 500 лет и находится в уникальном месте России, городе Гороховец, входящем в Золотое Кольцо страны.</p>
-                    <img src="img/prod_img1.jpg" alt="">
-                    <h3>Движение воды RusseQuelle происходит через пласты пород толщиной несколько сотен метров.</h3>
-                    <ol>
-                        <li>Движение – это способ познавать мир вокруг, соединять одни явления с другими и открывать что-то новое.</li>
-                        <li>Движение – это способ познавать мир вокруг, соединять одни явления с другими и открывать что-то новое.</li>
-                        <li>Движение – это способ познавать мир вокруг, соединять одни явления с другими и открывать что-то новое.</li>
-                    </ol>
-                </div>
-            </div>
-            <div class="right">
-                <div class="text">
-                    <h3>RusseQuelle - лучшая <br>минеральная вода 2015 года</h3>
-                    <img src="img/prod_img2.jpg" alt="">
-                    <p>В наши дни технология производства заключается лишь в угольной фильтрации воды и абсолютной чистоте на линии розлива. Для обеспечения высокого качества <a href="products.html">продукции</a> мы используем только экологически чистое сырье немецкого и российского производства.</p>
-                    <img src="img/prod_img3.jpg" alt="">
-                </div>
-            </div>
+
+            <?php // flexible content ?>
+            <?php get_template_part('_templates/_blocks/_flexible-content'); ?>
+            
             <div class="clear"></div>
         </div>
     </div>
-    <div id="about">
-        <!-- about start -->
-        <div id="about-slider">
-            <ul class="slides">
-                <li>
-                    <div class="bottle">
-                        <div class="table">
-                            <div class="cell">
-                                <p>SI</p>
-                                <span>Кремний: 5</span>
-                                <div class="hint">
-                                    <p>Кремний активизирует воду, меняет её структуру, выводит в осадок соединения тяжелых металлов, подавляет болезнетворную микрофлору. Кремень во много раз ускоряет окислительно-восстановительные реакции в воде, придавая ей целебные свойства.</p>
-                                </div>
+
+    <?php $active_4 = get_field('active_4', 8); if($active_4): // ABOUT WATER ?>
+        <section id="about">
+            <div id="about-slider">
+                <ul class="slides">
+                    <li>
+                        <div class="bottle">
+                            <div class="table">
+                                <?php for($n = 1; $n<=6; $n++): ?>
+                                    <div class="cell">
+                                        <?php $cell_title = get_field('cell_'.$n.'_title', 8); if($cell_title): ?>
+                                            <p><?php echo $cell_title; ?></p>
+                                        <?php endif; ?>
+                                        <?php $cell_subtitle = get_field('cell_'.$n.'_subtitle', 8); if($cell_subtitle): ?>
+                                            <span><?php echo $cell_subtitle; ?></span>
+                                        <?php endif; ?>
+                                        <?php $cell_hover = get_field('cell_'.$n.'_hover', 8); if($cell_hover): ?>
+                                            <div class="hint">
+                                                <p><?php echo $cell_hover; ?></p>
+                                            </div>
+                                        <?php endif; ?>
+                                    </div>
+                                <?php endfor; ?>
                             </div>
-                            <div class="cell">
-                                <p>CA</p>
-                                <span>Кальций: 68</span>
-                                <div class="hint">
-                                    <p>Кремний активизирует воду, меняет её структуру, выводит в осадок соединения тяжелых металлов, подавляет болезнетворную микрофлору.</p>
-                                </div>
-                            </div>
-                            <div class="cell">
-                                <p>NA</p>
-                                <span>Натрий: 10</span>
-                            </div>
-                            <div class="cell">
-                                <p>K</p>
-                                <span>Калий: 0,1</span>
-                                <div class="hint">
-                                    <p>Кремний активизирует воду, меняет её структуру, выводит в осадок соединения тяжелых металлов, подавляет болезнетворную микрофлору. Кремень во много раз ускоряет окислительно-восстановительные реакции в воде, придавая ей целебные свойства.</p>
-                                </div>
-                            </div>
-                            <div class="cell">
-                                <p>MG</p>
-                                <span>Магний: 29</span>
-                            </div>
-                            <div class="cell">
-                                <p>F</p>
-                                <span>Фтор: 0,3</span>
-                            </div>
-                            <div class="clear"></div>
                         </div>
-                    </div>
-                    <p class="text">*Анализ произведен Испытательной лабораторией минеральных вод Всероссийского научно-исследовательского института пивоваренной, безалкогольной и винодельческой промышленности Российской академии сельскохозяйственных наук.</p>
-                </li>
-                <li>
-                    <div class="bottle">
-                        <div class="table">
-                            <div class="cell">
-                                <p>SI</p>
-                                <span>Кремний: 5</span>
-                                <div class="hint">
-                                    <p>Кремний активизирует воду, меняет её структуру, выводит в осадок соединения тяжелых металлов, подавляет болезнетворную микрофлору. Кремень во много раз ускоряет окислительно-восстановительные реакции в воде, придавая ей целебные свойства.</p>
-                                </div>
-                            </div>
-                            <div class="cell">
-                                <p>CA</p>
-                                <span>Кальций: 68</span>
-                                <div class="hint">
-                                    <p>Кремний активизирует воду, меняет её структуру, выводит в осадок соединения тяжелых металлов, подавляет болезнетворную микрофлору. </p>
-                                </div>
-                            </div>
-                            <div class="cell">
-                                <p>NA</p>
-                                <span>Натрий: 10</span>
-                            </div>
-                            <div class="cell">
-                                <p>K</p>
-                                <span>Калий: 0,1</span>
-                                <div class="hint">
-                                    <p>Кремний активизирует воду, меняет её структуру, выводит в осадок соединения тяжелых металлов, подавляет болезнетворную микрофлору. Кремень во много раз ускоряет окислительно-восстановительные реакции в воде, придавая ей целебные свойства.</p>
-                                </div>
-                            </div>
-                            <div class="cell">
-                                <p>MG</p>
-                                <span>Магний: 29</span>
-                            </div>
-                            <div class="cell">
-                                <p>F</p>
-                                <span>Фтор: 0,3</span>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                    </div>
-                    <p class="text">*Анализ произведен Испытательной лабораторией минеральных вод Всероссийского научно-исследовательского института пивоваренной, безалкогольной и винодельческой промышленности Российской академии сельскохозяйственных наук.</p>
-                </li>
-            </ul>
-        </div>
-        <!-- about end -->
-    </div>
+                        <?php $text_4 = get_field('text_4', 8); if($text_4): ?>
+                            <p class="text"><?php echo $text_4; ?></p>
+                        <?php endif; ?>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    <?php endif; ?>
 
     <div class="bottom-line">
         <a href="/shop/" class="back"><?php echo __('To the catalog', 'RQ'); ?></a>
@@ -178,53 +138,6 @@
     <?php endwhile; get_footer( 'shop' ); ?>
 </section>
 
-
--------------------------------
-
-
-
-
-<?php
-global $product;
-$title = get_the_title();
-
-// prices
-$price = $product->get_price();
-
-// images
-$img = get_field('_thumbnail_id');
-$gallery = get_field('_gallery');
-
-// characters
-$buttle_volume = get_field('buttle_volume');
-$pack_count = get_field('pack_count');
-?>
-
-<div class="product">
-
-    <?php echo $title; ?>
-    <?php echo $price; ?> <?php echo get_woocommerce_currency_symbol(); ?>
-
-    <?php if(is_array($img) && count($img)): ?>
-        <img src='<?php echo $img['sizes']['600x0']; ?>' alt='<?php echo $title; ?>'>
-    <?php endif; ?>
-
-    <?php if(is_array($gallery) && count($gallery)): ?>
-        <?php foreach($gallery as $img): ?>
-            <img src='<?php echo $img['sizes']['600x0']; ?>' alt='<?php echo $title; ?>'>
-        <?php endforeach; ?>
-    <?php endif; ?>
-
-    <?php if($buttle_volume): ?>
-        <?php echo $buttle_volume; ?>
-    <?php endif; ?>
-
-    <?php if($pack_count): ?>
-        <?php echo $pack_count; ?>
-    <?php endif; ?>
-
-</div>
-
 <?php /* WRITE SCRIPTS HERE */ ?>
 <script>
     $(document).ready(function(){
@@ -233,11 +146,14 @@ $pack_count = get_field('pack_count');
             directionNav: false,
             controlNav: true
         });
+
+        <?php $active_4 = get_field('active_4', 8); if($active_4): // ABOUT WATER ?>
         $('#about-slider').flexslider({
             animation: "slide",
             directionNav: false,
             controlNav: true
         });
+        <?php endif; ?>
     });
 </script>
 
