ab6ed680883b79b73c0297dd7e52032957f9a72b
PetyaSh <petyash@i.ua>
2016-06-10 00:44:06 +0300

    no message

--- WP_RQ/themes/RQ/archive-gallery.php
+++ WP_RQ/themes/RQ/archive-gallery.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <?php global $wp_query; ?>
+<?php $posts_per_page = 9; // кол-ство на странице ?>
 
 <section id="gal-page">
     <div id="top"<?php $gallery_bg = get_field('gallery_bg', 32); if(is_array($gallery_bg) && count($gallery_bg)): ?> style="background: url('<?php echo $gallery_bg['sizes']['large']; ?>') no-repeat 50% 50%;"<?php endif; ?>>
@@ -45,16 +46,40 @@
         </ul>
         <?php endif; ?>
 
-        <?php if(have_posts() ) : ?>
-        <ul class="list">
-            <?php while (have_posts()) : the_post(); $type = get_field('type'); ?>
-            <li<?php if($type==8): ?> class="video"<?php endif; ?>>
-                <?php get_template_part('_templates/_archive-gallery'); ?>
-                <img src="<?php echo get_template_directory_uri(); ?>/img/ui/4x3.png" alt="">
-            </li>
+        <?php $arr_gallery = array(); if(have_posts() ) : // собираем массив ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <?php $arr_gallery[] = get_the_ID(); ?>
             <?php endwhile; wp_reset_query(); ?>
+        <?php endif; ?>
+
+        <?php $paged_arr = array(); if(count($arr_gallery)): // разбиваем его на страницы ?>
+            <?php $paged_arr = array_chunk($arr_gallery, $posts_per_page); ?>
+        <?php endif; ?>
+
+        <?php if(is_array($paged_arr) && count($paged_arr)): // вывод ?>
+        <ul class="list">
+            <?php foreach($paged_arr as $p=>$ids): ?>
+                <?php foreach($ids as $id): ?>
+                    <?php
+                    $type = get_field('type', $id);
+                    $img = get_field('img', $id);
+                    $video_code = get_field('video_code', $id);
+                    ?>
+                    <li class="page_<?php echo ($p+1); ?><?php if($type==8): ?> video<?php endif; ?><?php if($p): ?> hidden<?php endif; ?>">
+                        <div class="bg"<?php if(is_array($img) && count($img)): ?> style="background-image:url('<?php echo $img['sizes']['430x320']; ?>')"<?php endif; ?>>
+                            <?php if($type==8): // видео ?>
+                                <?php if($video_code): ?><a href="http://www.youtube.com/v/<?php echo $video_code; ?>?fs=1&amp;autoplay=1" class="various fancybox.iframe"></a><?php endif; ?>
+                            <?php elseif($type==7): // фото ?>
+                                <?php if(is_array($img) && count($img)): ?><a href="<?php echo $img['sizes']['large']; ?>" class="fancy" rel="gal"></a><?php endif; ?>
+                            <?php endif; ?>
+                        </div>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/ui/4x3.png" alt="">
+                    </li>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
         </ul>
         <?php endif; ?>
+
         <div class="clear"></div>
     </div>
 
@@ -99,7 +124,19 @@
     $(function() {
         $('.paging').pagination({
             items: <?php echo $wp_query->post_count; ?>,
-            itemsOnPage: 1
+            itemsOnPage: <?php echo $posts_per_page; ?>,
+            displayedPages: 5,
+            edges: 1,
+            cssStyle: '',
+            onPageClick: function(pageNumber, event){
+                $('.list li').each(function(){
+                    $(this).hide();
+                    if($(this).hasClass('page_' + pageNumber )){
+                        $(this).fadeIn();
+                    }
+                });
+                return false;
+            }
         });
     });
 </script>
